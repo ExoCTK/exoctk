a8a5796a0ec25fb620caa2de8d500c5c21f63c19
Matthew Hill <mhill@stsci.edu>
2017-01-05 15:08:25 -0500

    get rid of exotransmit package

--- ExoCTK/exotransmit/setup_package.py
+++
@@ -1,5 +0,0 @@
-from distutils.extension import Extension
-
-def get_extensions():
-    return [Extension(name='ExoCTK.exotransmit', sources=['ExoCTK/exotransmit/exotransmit.pyx'],
-                     include_dirs=['numpy', 'ExoCTK/exotransmit/include'])]
