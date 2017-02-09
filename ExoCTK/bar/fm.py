df7fcaab8648d5def3a0e16fe0a94af2c0ffd65e
Joe Filippazzo <jfilippazzo@stsci.edu>
2017-02-09 12:24:35 -0500

    Tried adding config parser to function that propagates the external file path.

--- ExoCTK/bar/fm.py
+++ ExoCTK/bar/fm.py
@@ -5,6 +5,7 @@ import os
 import math
 import numpy as np
 import scipy as sp
+from .. import version
 from array import *
 from scipy import interpolate
 from scipy import signal
@@ -17,7 +18,7 @@ import datetime
 from pickle import *
 from numba import jit
 
-DATA_DIR = os.path.dirname(__file__)
+DATA_DIR = version.external_files()
 #Computing transmission spectrum----------------------
 #uses correlated-K treatment of opacities
 @jit
