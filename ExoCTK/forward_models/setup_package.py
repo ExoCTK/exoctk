0cb11b51a59cd2915fcee1cae194af80fb91a10f
Jules Fowler <jfowler@stsci.edu>
2018-03-13 10:36:43 -0400

    Missed these PALs.

--- ExoCTK/forward_models/setup_package.py
+++ ExoCTK/forward_models/setup_package.py
@@ -3,13 +3,13 @@ import os
 
 def get_extensions():
     cfiles = []
-    for f in os.listdir('ExoCTK/pal/include/'):
+    for f in os.listdir('ExoCTK/forward_models/include/'):
         if f.endswith('.c') and f != 'main_transmission.c':
-            cfiles.append(os.path.join('ExoCTK/pal/include/', f))
+            cfiles.append(os.path.join('ExoCTK/forward_models/include/', f))
 
     return [Extension(name='ExoCTK.pal._exotransmit_wrapper',
-                      sources=['ExoCTK/pal/_exotransmit_wrapper.pyx']+cfiles,
-                      include_dirs=['numpy', 'ExoCTK/pal/include'])]
+                      sources=['ExoCTK/forward_models/_exotransmit_wrapper.pyx']+cfiles,
+                      include_dirs=['numpy', 'ExoCTK/forward_models/include'])]
 
 def get_package_data():
-    return {'ExoCTK.pal': ['include/*']}
+    return {'ExoCTK.forward_models': ['include/*']}
