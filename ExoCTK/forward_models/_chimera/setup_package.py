07319fdc9acfcb74d2612665543113a4e3376749
Jules Fowler <jfowler@stsci.edu>
2018-03-13 11:06:56 -0400

    One more setup file to change.

--- ExoCTK/forward_models/_chimera/setup_package.py
+++ ExoCTK/forward_models/_chimera/setup_package.py
@@ -1,8 +1,11 @@
 from distutils.extension import Extension
 
 def get_extensions():
-    return [Extension(name='ExoCTK.pal._chimera._tran_module', sources=["ExoCTK/pal/_chimera/src/_tau.c", "ExoCTK/pal/_chimera/src/_tau_wrap.c", "ExoCTK/pal/_chimera/src/_xsects.c"],
-                     include_dirs=['numpy', 'ExoCTK/pal/_chimera/include'])]
+    return [Extension(name='ExoCTK.forward_models._chimera._tran_module',
+        sources=["ExoCTK/forward_models/_chimera/src/_tau.c",
+            "ExoCTK/forward_models/_chimera/src/_tau_wrap.c",
+            "ExoCTK/forward_models/_chimera/src/_xsects.c"],
+                     include_dirs=['numpy', 'ExoCTK/forward_models/_chimera/include'])]
 
 def get_package_data():
-    return {'ExoCTK.pal._chimera': ['include/*', 'data/*']}
+    return {'ExoCTK.forward_models._chimera': ['include/*', 'data/*']}
