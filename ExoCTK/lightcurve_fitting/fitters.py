205da08b8af297824e7d704d48ad596979aa9797
Joe Filippazzo <jfilippazzo@stsci.edu>
2018-06-11 12:49:02 -0400

    Got composite model fitting working.

--- ExoCTK/lightcurve_fitting/fitters.py
+++ ExoCTK/lightcurve_fitting/fitters.py
@@ -7,10 +7,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import batman
 import lmfit
+import copy
 
 from .models import Model
+from .parameters import Parameters
     
-def lmfitter(data, model, unc=None, method='leastsq', verbose=True):
+def lmfitter(time, data, model, unc=None, method='leastsq', verbose=True):
     """Use lmfit
     
     Parameters
@@ -30,9 +32,13 @@ def lmfitter(data, model, unc=None, method='leastsq', verbose=True):
     # Initialize lmfit Params object
     initialParams = lmfit.Parameters()
     
+    # Concatenate the lists of parameters
+    all_params = [i for j in [model.components[n].parameters.list for n in range(len(model.components))] for i in j]
+    
+    # Group the different variable types
     param_list = []
     indep_vars = {}
-    for param in model.parameters.list:
+    for param in all_params:
         param = list(param)
         if param[2] == 'free':
             param[2] = True
@@ -42,13 +48,14 @@ def lmfitter(data, model, unc=None, method='leastsq', verbose=True):
             param_list.append(tuple(param))
         else:
             indep_vars[param[0]] = param[1]
+            
+    # Add the time as an independent variable
+    indep_vars['time'] = time
 
-    # Get values from input ExoCTK.lightcurve_fitting.parameters.Parameters instance
-    # Format: (key, value, vary?, min, max)
+    # Get values from input ExoCTK.lightcurve_fitting.parameters.Parameters instances
     initialParams.add_many(*param_list)
         
     # Create the lightcurve model
-    # lcmodel = lmfit.Model(model, independent_vars=['times', 'ldtype', 'transitType'])
     lcmodel = lmfit.Model(model.eval)
     lcmodel.independent_vars = indep_vars.keys()
     
@@ -60,8 +67,21 @@ def lmfitter(data, model, unc=None, method='leastsq', verbose=True):
     result = lcmodel.fit(data, weights=1/unc, params=initialParams, method=method, **indep_vars)
     if verbose:
         print(result.fit_report())
+
+    # Get the best fit params
+    fit_params = result.__dict__['params']
+    new_params = [(fit_params.get(i).name, fit_params.get(i).value, fit_params.get(i).vary, fit_params.get(i).min, fit_params.get(i).max) for i in fit_params]
     
     # Create new model with best fit parameters
-    best_fit = Model(time=model.time, flux=result.best_fit, name='Best Fit')
+    params = Parameters()
     
-    return best_fit
+    # Try to store each as an attribute
+    for param in new_params:
+        setattr(params, param[0], param[1:])
+
+    # Make a new model instance
+    best_model = copy.copy(model)
+    best_model.name = 'Best Fit'
+    best_model.parameters = params
+    
+    return best_model
