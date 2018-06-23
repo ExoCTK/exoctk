b69249f5b273cf8fffb71eabbbb44c0b1a88c426
Joe Filippazzo <jfilippazzo@stsci.edu>
2018-06-22 21:13:03 -0400

    models.py PEP8 compliance.

--- ExoCTK/lightcurve_fitting/models.py
+++ ExoCTK/lightcurve_fitting/models.py
@@ -4,6 +4,7 @@ used to fit light curves
 Author: Joe Filippazzo
 Email: jfilippazzo@stsci.edu
 """
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 import astropy.units as q
@@ -28,42 +29,40 @@ class Model:
         self._parameters = None
         self.components = None
         self.fmt = None
-        
+
         # Store the arguments as attributes
         for arg, val in kwargs.items():
             setattr(self, arg, val)
 
-
     def __mul__(self, other):
         """Multiply model components to make a combined model
-        
+
         Parameters
         ----------
         other: ExoCTK.lightcurve_fitting.models.Model
             The model to multiply
-        
+
         Returns
         -------
         ExoCTK.lightcurve_fitting.lightcurve.Model
             The combined model
         """
         # Make sure it is the right type
-        if not all([hasattr(other, attr) for attr in ['units','flux','time']]):
+        attrs = ['units', 'flux', 'time']
+        if not all([hasattr(other, attr) for attr in attrs]):
             raise TypeError('Only another Model instance may be multiplied.')
-        
+
         return CompositeModel([copy.copy(self), other])
-        
-        
+
     @property
     def flux(self):
         """A getter for the flux"""
         return self._flux
-        
-        
+
     @flux.setter
     def flux(self, flux_array):
         """A setter for the flux
-        
+
         Parameters
         ----------
         flux_array: sequence
@@ -72,14 +71,13 @@ class Model:
         # Check the type
         if not isinstance(flux_array, (np.ndarray, tuple, list)):
             raise TypeError("flux axis must be a tuple, list, or numpy array.")
-        
+
         # Set the array
         self._flux = np.array(flux_array)
-        
-        
+
     def interp(self, new_time):
         """Interpolate the flux to a new time axis
-        
+
         Parameters
         ----------
         new_time: sequence, astropy.units.quantity.Quantity
@@ -87,20 +85,18 @@ class Model:
         """
         # Check the type
         if not isinstance(new_time, (np.ndarray, tuple, list)):
-            raise TypeError("Time axis must be a tuple, list, or numpy array.")
-            
+            raise TypeError("Time axis must be a tuple, list, or numpy array")
+
         # Calculate the new flux
         self.flux = np.interp(new_time, self.time, self.flux)
-        
+
         # Set the new time axis
         self.time = new_time
-        
-        
+
     @property
     def parameters(self):
         """A getter for the parameters"""
         return self._parameters
-
 
     @parameters.setter
     def parameters(self, params):
@@ -108,18 +104,18 @@ class Model:
         # Process if it is a parameters file
         if isinstance(params, str) and os.file.exists(params):
             params = Parameters(params)
-            
+
         # Or a Parameters instance
         if not isinstance(params, (Parameters, type(None))):
-            raise TypeError("'params' argument must be a JSON file, ascii file, or ExoCTK.lightcurve_fitting.parameters.Parameters instance.")
-            
+            raise TypeError("'params' argument must be a JSON file, ascii\
+                             file, or parameters.Parameters instance.")
+
         # Set the parameters attribute
         self._parameters = params
-        
-        
+
     def plot(self, time, components=False, **kwargs):
         """Plot the model
-        
+
         Parameters
         ----------
         components: bool
@@ -127,33 +123,30 @@ class Model:
         """
         # Set the time
         self.time = time
-        
+
         flux = self.eval(**kwargs)
         if self.fmt is not None:
             plt.plot(self.time, flux, self.fmt, label=self.name)
         else:
             plt.plot(self.time, flux, label=self.name)
-        
+
         if components and self.components is not None:
             for comp in self.components:
                 flux = comp.plot(self.time, **kwargs)
-            
+
         plt.xlabel(self.units)
         plt.ylabel('Flux')
-        
         plt.legend(loc=0, frameon=False)
-        
-        
+
     @property
     def time(self):
         """A getter for the time"""
         return self._time
-        
-        
+
     @time.setter
     def time(self, time_array, units='MJD'):
         """A setter for the time
-        
+
         Parameters
         ----------
         time_array: sequence, astropy.units.quantity.Quantity
@@ -164,24 +157,22 @@ class Model:
         # Check the type
         if not isinstance(time_array, (np.ndarray, tuple, list)):
             raise TypeError("Time axis must be a tuple, list, or numpy array.")
-        
+
         # Set the units
         self.units = units
-        
+
         # Set the array
         self._time = time_array
-        
-        
+
     @property
     def units(self):
         """A getter for the units"""
         return self._units
-        
-        
+
     @units.setter
     def units(self, units):
         """A setter for the units
-        
+
         Parameters
         ----------
         units: str
@@ -190,7 +181,7 @@ class Model:
         # Check the type
         if units not in ['BJD', 'MJD', 'phase']:
             raise TypeError("units axis must be 'BJD', 'MJD', or 'phase'.")
-            
+
         self._units = units
 
 
@@ -198,7 +189,7 @@ class CompositeModel(Model):
     """A class to create composite models"""
     def __init__(self, models, **kwargs):
         """Initialize the composite model
-        
+
         Parameters
         ----------
         models: sequence
@@ -206,23 +197,23 @@ class CompositeModel(Model):
         """
         # Inherit from Model calss
         super().__init__(**kwargs)
-        
+
         # Store the models
         self.components = models
-        
+
     def eval(self, **kwargs):
         """Evaluate the model components"""
         # Get the time
         if self.time is None:
             self.time = kwargs.get('time')
-        
+
         # Empty flux
         flux = 1.
-        
+
         # Evaluate flux at each model
         for model in self.components:
             flux *= model.eval(**kwargs)
-            
+
         return flux
 
 
@@ -233,54 +224,53 @@ class PolynomialModel(Model):
         """
         # Inherit from Model class
         super().__init__(**kwargs)
-        
+
         # Check for Parameters instance
         self.parameters = kwargs.get('parameters')
-            
+
         # Generate parameters from kwargs if necessary
         if self.parameters is None:
             self._parse_coeffs(kwargs)
-        
-            
+
     def _parse_coeffs(self, coeff_dict):
         """Convert dict of 'c#' coefficients into a list
         of coefficients in decreasing order, i.e. ['c2','c1','c0']
-        
+
         Parameters
         ----------
         coeff_dict: dict
             The dictionary of coefficients
-        
+
         Returns
         -------
         np.ndarray
             The sequence of coefficient values
         """
-        params = {cN:coeff for cN,coeff in coeff_dict.items() if cN.startswith('c') and cN[1:].isdigit()}
+        params = {cN: coeff for cN, coeff in coeff_dict.items()
+                  if cN.startswith('c') and cN[1:].isdigit()}
         self.parameters = Parameters(**params)
-        
+
         # Parse 'c#' keyword arguments as coefficients
         coeffs = np.zeros(100)
-        for k,v in self.parameters.dict.items():
+        for k, v in self.parameters.dict.items():
             if k.lower().startswith('c') and k[1:].isdigit():
                 coeffs[int(k[1:])] = v[0]
-           
+
         # Trim zeros and reverse
         self.coeffs = np.trim_zeros(coeffs)[::-1]
-        
-        
+
     def eval(self, **kwargs):
         """Evaluate the function with the given values"""
         # Get the time
         if self.time is None:
             self.time = kwargs.get('time')
-        
+
         # Create the polynomial from the coeffs
         poly = np.poly1d(self.coeffs)
-        
+
         # Convert to local time
-        time_local = self.time-self.time.mean()
-        
+        time_local = self.time - self.time.mean()
+
         # Evaluate the polynomial
         return np.polyval(poly, time_local)
 
@@ -292,41 +282,42 @@ class TransitModel(Model):
         """
         # Inherit from Model calss
         super().__init__(**kwargs)
-        
+
         # Check for Parameters instance
         self.parameters = kwargs.get('parameters')
-        
+
         # Generate parameters from kwargs if necessary
         if self.parameters is None:
             self.parameters = Parameters(**kwargs)
-            
+
         # Store the ld_profile
         self.ld_func = ld_profile(self.parameters.limb_dark.value)
-        self.coeffs = ['u{}'.format(n) for n in range(len(inspect.signature(self.ld_func).parameters))[1:]]
-        
-        
+        len_params = len(inspect.signature(self.ld_func).parameters)
+        self.coeffs = ['u{}'.format(n) for n in range(len_params)[1:]]
+
     def eval(self, **kwargs):
         """Evaluate the function with the given values"""
         # Get the time
         if self.time is None:
             self.time = kwargs.get('time')
-        
+
         # Generate with batman
         bm_params = batman.TransitParams()
-        
+
         # Set all parameters
         for p in self.parameters.list:
             setattr(bm_params, p[0], p[1])
-        
+
         # Combine limb darkening coeffs
         bm_params.u = [getattr(self.parameters, u).value for u in self.coeffs]
-        
+
         # Use batman ld_profile name
-        if self.parameters.limb_dark.value=='4-parameter':
+        if self.parameters.limb_dark.value == '4-parameter':
             bm_params.limb_dark = 'nonlinear'
-        
+
         # Make the eclipse
-        m_eclipse = batman.TransitModel(bm_params, self.time, transittype=self.parameters.transittype.value)
+        tt = self.parameters.transittype.value
+        m_eclipse = batman.TransitModel(bm_params, self.time, transittype=tt)
 
         # Evaluate the light curve
         return m_eclipse.light_curve(bm_params)
