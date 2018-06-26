435ca6995ff9e38dee99f0b798b04b4b4f345a4a
Joe Filippazzo <jfilippazzo@stsci.edu>
2018-06-26 14:35:27 -0400

    Added lmfit dependency

--- setup.py
+++ setup.py
@@ -117,7 +117,7 @@ package_info['package_data'][PACKAGENAME].extend(ext_files)
 # https://groups.google.com/forum/#!topic/astropy-dev/urYO8ckB2uM
 
 install_requires = ['numpy', 'scipy', 'cython', 'matplotlib', 'numba', 'pysynphot', 'sphinx_automodapi',
-                    'sphinx_rtd_theme', 'bibtexparser', 'bokeh', 'batman-package', 'pandas']
+                    'sphinx_rtd_theme', 'bibtexparser', 'bokeh', 'batman-package', 'pandas', 'lmfit']
 
 setup(name=PACKAGENAME,
       version=VERSION,
