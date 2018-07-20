4a299977b83404f555ef098ba67f084347d457aa
Joe Filippazzo <jfilippazzo@stsci.edu>
2018-07-20 15:45:33 -0400

    Made resolve.py PEP8 compliant

--- ExoCTK/contam_visibility/resolve.py
+++ ExoCTK/contam_visibility/resolve.py
@@ -1,18 +1,27 @@
-import numpy as np
-from astroquery.irsa import Irsa
 from astroquery.simbad import Simbad
-import astropy.units as u	
+
 
 def resolve_target(targetName):
-	try:
-		target_info = Simbad.query_object(targetName)
-		targetRA = target_info['RA']
-		targetDEC = target_info['DEC']
-		ra = (targetRA[0].replace(' ', ':'))
-		dec = (targetDEC[0].replace(' ', ':'))
-		return ra, dec
-		
-	except:
-		ra = 'unresolved'
-		dec = 'unresolved'
-		return ra, dec
+    """Resolve a target in SIMBAD
+
+    Parameters
+    ----------
+    targetName: str
+        The name of the target
+
+    Returns
+    -------
+    tuple
+        The ra and dec of the target"""
+    try:
+        target_info = Simbad.query_object(targetName)
+        targetRA = target_info['RA']
+        targetDEC = target_info['DEC']
+        ra = (targetRA[0].replace(' ', ':'))
+        dec = (targetDEC[0].replace(' ', ':'))
+        return ra, dec
+
+    except IndexError:
+        ra = 'unresolved'
+        dec = 'unresolved'
+        return ra, dec
