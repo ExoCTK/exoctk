6311ffc88b96252d4caea908a8e5711ccb133c01
Joe Filippazzo <jfilippazzo@stsci.edu>
2018-10-18 15:35:08 -0400

    Put lmfit back in.

--- ci/setup_conda_env.sh
+++ ci/setup_conda_env.sh
@@ -8,5 +8,5 @@ conda env list
 echo "Installing packages..."
 pip install numpy astropy
 conda install numpy astropy scipy cython matplotlib numba mock bokeh h5py sphinx pandas
-pip install bibtexparser astroquery svo_filters batman-package
+pip install bibtexparser astroquery svo_filters batman-package lmfit
 echo $PATH
