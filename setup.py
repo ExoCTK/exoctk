f2df7b1f173f4c7aafaffd31e858f14b3c5c64bb
Joe Filippazzo <jfilippazzo@stsci.edu>
2018-10-18 11:53:01 -0400

    please please please please please pretty please please

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 
 setup(name='ExoCTK',
       version=0.2,
-      description='Stuff',
+      description='Observation reduction and planning tools for exoplanet science',
       install_requires=['numpy', 'astropy', 'scipy', 'cython', 'matplotlib', 'numba', 'pysynphot', 'sphinx_automodapi', 'sphinx_rtd_theme', 'bibtexparser', 'bokeh', 'batman-package', 'pandas', 'lmfit', 'svo_filters', 'sphinx_astropy'],
-      author='Joe Filippazzo and Jonathan Fraine',
+      author='The ExoCTK Group',
       author_email='jfilippazzo@stsci.edu',
       license='MIT',
-      url='https://github.com/spacetelescope/awesimsoss',
-      long_description='More stuff',
+      url='https://github.com/ExoCTK/ExoCTK',
+      long_description='',
       zip_safe=False,
       use_2to3=False
 )
