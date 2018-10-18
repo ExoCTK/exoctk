b51a2e7b96e06fa370a145948c1e8d647389d3ec
Joe Filippazzo <jfilippazzo@stsci.edu>
2018-10-18 11:01:04 -0400

    please

--- ci/setup_conda_env.sh
+++ ci/setup_conda_env.sh
@@ -5,6 +5,6 @@ conda create -n exoctk python=$PYTHON_VERSION || exit 1
 source activate exoctk
 
 echo "Installing packages..."
-conda install numpy scipy cython matplotlib numba mock bokeh h5py sphinx pandas lmfit
+conda install numpy astropy scipy cython matplotlib numba mock bokeh h5py sphinx pandas lmfit
 pip install numpy astropy bibtexparser astroquery svo_filters
 pip install batman-package
