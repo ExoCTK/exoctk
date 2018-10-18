efcd761cfb6eb5c630cddb838dfad4a3a44fbb8a
Joe Filippazzo <jfilippazzo@stsci.edu>
2018-10-18 10:48:17 -0400

    Trying something different with build.

---
+++ ci/setup_conda_env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Creating a Python $PYTHON_VERSION environment"
+conda create -n exoctk python=$PYTHON_VERSION || exit 1
+source activate exoctk
+
+echo "Installing packages..."
+conda install numpy scipy cython matplotlib numba mock bokeh h5py sphinx pandas lmfit
+pip install astropy bibtexparser batman-package astroquery svo_filters
