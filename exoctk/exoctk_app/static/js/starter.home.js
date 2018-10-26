00c6fdf0f8038c35e16ff83f2fc37582603ff563
Joe Filippazzo <jfilippazzo@stsci.edu>
2018-10-26 15:44:01 -0400

    Added ExoCTKWeb to package

---
+++ exoctk/exoctk_app/static/js/starter.home.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    if (!window.console)
+        window.console = {};
+
+    if (!window.console.log)
+        window.console.log = function() {};
+
+    // If we're on the main page, startup checking of the runs
+    $("#calculation-table tbody tr").each(function(i, row) {
+        var status_url = "/calculation/status/" + row.id.slice(1);
+        updateStatus(status_url);
+    });
+});
+
+function updateStatus(statusUrl) {
+    console.log("Updating status" + statusUrl);
+    // Query the status url for information about the state of the calculation
+    $.getJSON(statusUrl, function(response) {
+        $('#m' + response.id).replaceWith(response.html);
+        if ((response.state != 'finished') && (response.state != 'cancelled')) {
+            // Check in another 2 seconds
+            setTimeout(function() {
+                updateStatus(statusUrl);
+            }, 2000);
+        }
+    });
+}
