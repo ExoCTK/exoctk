06ee726988548142aea0eb8d1f84a65770d4dc1d
Jules Fowler <jfowler@stsci.edu>
2018-10-30 17:23:44 -0400

    Integrations_groups --> groups_integrations.

--- exoctk/__init__.py
+++ exoctk/__init__.py
@@ -6,6 +6,6 @@ The Exoplanet Characterization Tool Kit is a collection of packages used to redu
 from .modelgrid import *
 from .utils import *
 from . import contam_visibility
-from . import integrations_groups
+from . import groups_integrations
 from . import limb_darkening
 from . import lightcurve_fitting
