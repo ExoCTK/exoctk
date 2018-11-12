b47cfa3a5f98a80d17ef32da70299ec065f94832
Joe Filippazzo <jfilippazzo@stsci.edu>
2018-11-12 16:35:21 -0500

    Updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='exoctk',
-      version='0.2.1',
+      version='0.2.2',
       description='Observation reduction and planning tools for exoplanet science',
       install_requires=['numpy', 'astropy', 'scipy', 'cython', 'matplotlib', 'numba', 'pysynphot', 'sphinx_automodapi', 'sphinx_rtd_theme', 'bibtexparser', 'bokeh', 'pandas', 'svo_filters', 'sphinx_astropy', 'batman-package', 'lmfit', 'flask'],
       author='The ExoCTK Group',
