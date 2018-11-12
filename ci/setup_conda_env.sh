5e33c3686e2a44414d11b5d620f29973f7f0f5f4
Joe Filippazzo <jfilippazzo@stsci.edu>
2018-11-12 11:50:35 -0500

    Added directory chmod and test script call.

--- ci/setup_conda_env.sh
+++ ci/setup_conda_env.sh
@@ -3,10 +3,8 @@
 echo "Creating a Python $PYTHON_VERSION environment"
 conda create -n exoctk python=$PYTHON_VERSION || exit 1
 source activate exoctk
-conda env list
 
 echo "Installing packages..."
 pip install numpy astropy
 conda install numpy astropy scipy cython matplotlib numba mock bokeh h5py sphinx pandas flask
 pip install bibtexparser astroquery svo_filters==0.2.16 batman-package lmfit
-echo $PATH
