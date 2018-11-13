2521a30be6382ac600e6b6eed08ba5163625171d
Joe Filippazzo <jfilippazzo@stsci.edu>
2018-11-13 10:02:40 -0500

    Added pytest dependency

--- ci/setup_conda_env.sh
+++ ci/setup_conda_env.sh
@@ -7,4 +7,4 @@ source activate exoctk
 echo "Installing packages..."
 pip install numpy astropy
 conda install numpy astropy scipy cython matplotlib numba mock bokeh h5py sphinx pandas flask
-pip install bibtexparser astroquery svo_filters==0.2.16 batman-package lmfit
+pip install bibtexparser astroquery svo_filters==0.2.16 batman-package lmfit pytest
