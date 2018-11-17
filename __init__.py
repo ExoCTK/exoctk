15b39b54f95e1948f4d2dcef9e1aa492b34b0286
Joe Filippazzo <jfilippazzo@stsci.edu>
2018-11-16 23:46:42 -0500

    Fixed relative imports

--- __init__.py
+++
@@ -1,7 +0,0 @@
-from .exoctk import modelgrid
-from .exoctk import references
-from .exoctk import utils
-from .exoctk import contam_visibility
-from .exoctk import groups_integrations
-from .exoctk import limb_darkening
-from .exoctk import lightcurve_fitting
--- exoctk/__init__.py
+++ exoctk/__init__.py
@@ -3,3 +3,10 @@
 """
 The Exoplanet Characterization Tool Kit is a collection of packages used to reduce and analyze observations of transiting exoplanets
 """
+from . import modelgrid
+from . import references
+from . import utils
+from . import contam_visibility
+from . import groups_integrations
+from . import limb_darkening
+from . import lightcurve_fitting
