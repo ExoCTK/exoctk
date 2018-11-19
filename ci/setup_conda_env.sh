93b020ed5c1df66604b0c7b70169fc6f74f15754
Matthew Bourque <bourque@stsci.edu>
2018-11-19 11:50:38 -0500

    Updated travis CI to use new environment.yml file instead of manually creating new conda environment.

--- ci/setup_conda_env.sh
+++ ci/setup_conda_env.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-echo "Creating a Python $PYTHON_VERSION environment"
-conda create -n exoctk python=$PYTHON_VERSION || exit 1
+echo "Creating conda environment"
+conda env create -f environment.yml || exit 1
 source activate exoctk
-
-echo "Installing packages..."
-pip install numpy astropy
-conda install numpy astropy scipy cython matplotlib numba mock bokeh h5py sphinx pandas flask
-pip install bibtexparser astroquery svo_filters==0.2.16 batman-package lmfit pytest
