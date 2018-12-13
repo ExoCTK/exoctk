8eb5c229044dc5bc72068104640d54a5178ca146
Mees Fix <mfix@stsci.edu>
2018-12-13 14:41:59 -0500

    added docopt

--- exoctk/phase_constraint_overlap/phase_constraint_overlap.py
+++ exoctk/phase_constraint_overlap/phase_constraint_overlap.py
@@ -1,7 +1,26 @@
+"""Phase contraint overlap tool. This tool calculates the minimum and maximum phase of
+the transit based on parameters provided by the user.
+
+Usage:
+  calculate_constraint <target_name> [--t_start=<t0>] [--period=<p>] [--obs_duration=<obs_dur>] [--transit_duration=<trans_dur>] [--window_size=<win_size>]
+  
+Arguments:
+  <target_name>                     Name of target
+Options:
+  -h --help                         Show this screen.
+  --version                         Show version.
+  --t_start=<t0>                    The starting time of the transit in BJD or HJD.
+  --period=<p>                      The period of the transit in days.
+  --obs_duration=<obs_dur>          The duration of the observation in hours.
+  --transit_duration=<trans_dur>    The duration of the transit in hours.
+  --window_size=<win_size>          The window size of the transit in hours.
+"""
+
 import math
 import os
 
 import argparse
+from docopt import docopt
 import numpy as np
 import requests
 import urllib
@@ -15,16 +34,21 @@ def build_target_url(target_name):
     target_url = "https://exo.mast.stsci.edu/api/v0.1/exoplanets/{}/properties/".format(encode_target_name)
     return target_url
 
-def calculate_phase(period, t0, obsDur, winSize):
-    minphase = 1.0 -((obsDur + winSize)/2.0/24/period)
-    maxphase = 1.0 -((obsDur - winSize)/2.0/24/period)
+def calculate_phase(period, t0, obs_duration, window_size=1.0):
+    print("CALCULATING PHASE CONSTRATIONS WITH VALUES: Period: {}, t0: {}, obs_duration: {}, window_size: {}".format(period, t0, obs_duration, window_size))
+    minphase = 1.0 -((obs_duration + window_size)/2.0/24/period)
+    maxphase = 1.0 -((obs_duration - window_size)/2.0/24/period)
     
     return minphase, maxphase
 
 def get_canonical_name(target_name):
     """Get ExoMAST prefered name for exoplanet.
     """
+
+    # Generalized target url
     target_url = "https://exo.mast.stsci.edu/api/v0.1/exoplanets/identifiers/"
+    
+    # Create params dict for url parsing. Easier than trying to format yourself.
     params = {"name":target_name}
     
     r = requests.get(target_url, params=params)
@@ -36,7 +60,6 @@ def get_canonical_name(target_name):
 def get_transit_details(target_name):
     '''send request to exomast restful api for target information.
     '''
-
     canonical_name = get_canonical_name(target_name)
 
     target_url = build_target_url(canonical_name)
@@ -48,19 +71,19 @@ def get_transit_details(target_name):
     else:
         print('Whoops, no data for this target!')
 
-    for item in target_data:
-         print(item['catalog_name'], item['orbital_period'])
+    # Some exoplanets have multiple catalog entries
+    # Temporary... Need to write a parser to select catalog
+    target_data = target_data[0]
+    
+    period = target_data['orbital_period']
+    t0 = target_data['transit_time']
+    transit_duration = target_data['transit_duration']
+    obs_duration = np.min((6, 3*transit_duration + 1))
 
-    # NExSci has more up-to-date options for exoplanet data so we will use that
-    # catalog by default.
-    # cats = [d['catalog_name'] for d in target_data if 'nexsci' in d]
-    # print(cats)
-
-    # are t0, obsDur, and winSize available via ExoMAST api?
-    # return period, t0, obsDur, winSize 
+    return period, t0, obs_duration
 
 
-def phase_overlap_constraint(period, t0, obsDur, winSize, target_name):
+def phase_overlap_constraint(target_name, period=None, t0=None, obs_duration=None, window_size=None):
     ''' The main function to calculate the phase overlap constraints.
         We will update to allow a user to just plug in the target_name 
         and get the other variables.
@@ -71,38 +94,38 @@ def phase_overlap_constraint(period, t0, obsDur, winSize, target_name):
             The period of the transit in days. 
         t0 : float
             The start time in BJD or HJD.
-        obsdur : float
+        obs_duration : float
             The duration of the observation in hours.
         winSize : float
             The window size of transit in hours. Default is 1 hour.
         target_name : string
             The name of the target transit. 
-
+        
         Returns
         -------
         minphase : float
             The minimum phase constraint.
         maxphase : float
-            The maximum phase constraint. '''
+            The maximum phase constraint.'''
 
-    if target_name == target_name:
-        print("Using the default values:")
-        print(target_name)
-        print(parser.get_default('target_name'))
-        minphase, maxphase = calculate_phase(period, t0, obsDur, winSize)
-
-    elif target_name != target_name and period != period or t0 != t0 or obsDur != obsDur or winSize != winSize:
-        print(target_name)
-        print(parser.get_default('target_name'))
-        print("Using your input values for calculations:")
-        minphase, maxphase = calculate_phase(period, t0, obsDur, winSize)
-
+    # If user only gave target_name.
+    if all(arg is None for arg in [period, t0, obs_duration, window_size]):
+        period, t0, obs_duration = get_transit_details(target_name)
+        minphase, maxphase = calculate_phase(period, t0, obs_duration)
+    # If user doesn't provide observation details but wants to changes window size
+    elif all(arg is None for arg in [period, t0, obs_duration]):
+        period, t0, obs_duration = get_transit_details(target_name)
+        minphase, maxphase = calculate_phase(period, t0, obs_duration, window_size)
+    # Make sure users are passing all transit information if providing their own.
+    # Can't supply period without t0 or obs_duration or any combination of these variables!
+    elif any(arg is None for arg in [period, t0, obs_duration]):
+        raise ValueError('If passing transit information, must include all values! period: {}, t0: {}, obs_duration: {}'.format(period, t0, obs_duration))
+    # Else user needs to provide all of the data, calculate using their values.
     else:
-        print("Have a different Target Name, but same defaults. Retriving variables from exoplanets.org for that target name:")
-        #period,t0,obsDur,winSize = get_transit_details(target_name)
-        minphase, maxphase = calculate_phase(period, t0, obsDur, winSize)
-    
-    print(minphase,maxphase)
+        minphase, maxphase = calculate_phase(period, t0, obs_duration, window_size)
+            
+    print("MINIMUM PHASE: {}, MAXIMUM PHASE: {}".format(minphase, maxphase))
+
 
 def parse_args():
     """Parses command line arguments.
@@ -122,26 +145,30 @@ def parse_args():
 
     # Add time arguments
     parser = argparse.ArgumentParser()
+    parser.add_argument('-n --target_name',
+        dest='target_name',
+        action='store',
+        type=str,
+        required=True,
+        help=target_name_help)
+    
+    parser.add_argument('-t --t_start',
+        dest='t0',
+        action='store',
+        type=float,
+        help=t0_help)
+
     parser.add_argument('-p --period',
         dest='period',
         action='store',
         type=float,
-        help=period_help,
-        default=3.7354845)
+        help=period_help)
     
-    parser.add_argument('-t --t0',
-        dest='t0',
-        action='store',
-        type=float,
-        help=t0_help,
-        default= 2454592.80154)
-
     parser.add_argument('-odur --obsDur',
         dest='obsDur',
         action='store',
         type=float,
-        help=obsDur_help,
-        default= 34447.38/3600.)
+        help=obsDur_help)
     
     parser.add_argument('-tdur --transitDur',
         dest='transitDur',
@@ -154,16 +181,9 @@ def parse_args():
         action='store',
         type=float,
         required=False,
-        help=winSize_help,
-        default=1.0)
+        help=winSize_help)
 
-    parser.add_argument('-n --target_name',
-        dest='target_name',
-        action='store',
-        type=str,
-        help=target_name_help,
-        default='WASP-17 b')
-
+    
     # Parse args
     args = parser.parse_args()
 
@@ -171,7 +191,9 @@ def parse_args():
     
 
 if __name__ == '__main__':
+    # args = parse_args()
+    args = docopt(__doc__, version='0.1')
 
-    args = parse_args()
-
-    phase_overlap_constraint(args.period, args.t0, args.obsDur, args.winSize, args.target_name)
+    phase_overlap_constraint(args['<target_name>'], args['--period'], 
+                             args['--t_start'], args['--transit_duration'], 
+                             args['--window_size'])
