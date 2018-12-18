2f073b22083cd6cc52471fd20603a54f62006ad8
Mees Fix <mfix@stsci.edu>
2018-12-18 13:49:43 -0500

    Added functionality to retrieve data from exomast for target resolver

--- exoctk/contam_visibility/resolve.py
+++ exoctk/contam_visibility/resolve.py
@@ -2,36 +2,7 @@ from exoctk.utils import get_target_data
 
 def resolve_target(targetName):
     data = get_target_data(targetName)
-    ra = data['ra']
-    dec = data['dec']
+    ra = data['RA']
+    dec = data['DEC']
 
     return ra, dec
-
-# from astroquery.simbad import Simbad
-
-
-# def resolve_target(targetName):
-#     """Resolve a target in SIMBAD
-
-#     Parameters
-#     ----------
-#     targetName: str
-#         The name of the target.
-
-#     Returns
-#     -------
-#     tuple
-#         The ra and dec of the target.
-#     """
-#     try:
-#         target_info = Simbad.query_object(targetName)
-#         targetRA = target_info['RA']
-#         targetDEC = target_info['DEC']
-#         ra = (targetRA[0].replace(' ', ':'))
-#         dec = (targetDEC[0].replace(' ', ':'))
-#         return ra, dec
-
-#     except IndexError:
-#         ra = 'unresolved'
-#         dec = 'unresolved'
-#         return ra, dec
