bbcfc9b4f97508f8302023f2effd860aa63c46f1
Matthew Bourque <bourque@stsci.edu>
2019-01-25 15:25:53 -0500

    Revert "Trying to glean more information about the installed conda environment via $PATH"
    This reverts commit e0fe9f1482ee87eef0fdab9204a87b5871f25ef6.

--- ci/setup_conda_env.sh
+++ ci/setup_conda_env.sh
@@ -3,5 +3,3 @@ echo "Creating conda environment for Python $PYTHON_VERSION"
 conda env create -f "environment-${PYTHON_VERSION}.yml" || exit 1
 export CONDA_ENV=exoctk-$PYTHON_VERSION
 source activate $CONDA_ENV
-echo "Activated $CONDA_ENV"
-echo $PATH
