c138b7b15b6ba6a55b78811fff0ecc9bea733340
Matthew Bourque <bourque@stsci.edu>
2019-01-25 10:29:05 -0500

    Trying to add gcc to dependencies to see if it solves batman installation issues

--- ci/setup_conda_env.sh
+++ ci/setup_conda_env.sh
@@ -4,6 +4,3 @@ echo "Creating conda environment for $PYTHON_VERSION"
 conda env create -f "environment-${PYTHON_VERSION}.yml" || exit 1
 export CONDA_ENV=exoctk-$PYTHON_VERSION
 source activate $CONDA_ENV
-
-git clone https://github.com/lkreidberg/batman.git
-python batman/setup.py install
