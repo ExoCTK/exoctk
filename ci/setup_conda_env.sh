a5cce07e6e9cb2ffd31bd4c0ce3f54569a02d9c3
Matthew Bourque <bourque@stsci.edu>
2019-01-28 16:48:52 -0500

    Moving environments to a separate folder so it doesnt clutter the parent directory too much

--- ci/setup_conda_env.sh
+++ ci/setup_conda_env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Creating conda environment for Python $PYTHON_VERSION"
-conda env create -f "environment-${PYTHON_VERSION}.yml" || exit 1
+conda env create -f "env/environment-${PYTHON_VERSION}.yml" || exit 1
 export CONDA_ENV=exoctk-$PYTHON_VERSION
 source activate $CONDA_ENV
