14620130f87cde3eea0c154da90b2ea88f2a3190
Matthew Bourque <bourque@stsci.edu>
2019-03-12 11:45:37 -0400

    Restructed class object for wrapper to make fit_info object available.  Removed some redundant function calls.

--- exoctk/atmospheric_retreivals/platon_wrapper.py
+++ exoctk/atmospheric_retreivals/platon_wrapper.py
@@ -90,12 +90,6 @@ def parse_parameter_file(parameter_file):
     with open(parameter_file, 'r') as f:
         params = yaml.load(f)
 
-    # Validate supplied parameters
-    validate_parameters(params)
-
-    # Apply proper multiplication factors
-    apply_factors(params)
-
     return params
 
 
@@ -143,48 +137,23 @@ class PlatonWrapper():
     """Class object for running the platon atmospheric retrieval
     software."""
 
-    def __init__(self, **params):
+    def __init__(self):
         """Initialize the class object."""
 
-        self.params = params
+        self.retriever = Retriever()
 
 
     def retrieve(self):
         """Perform the atmopsheric retreival."""
 
-        # Construct fit_info object
-        retriever = Retriever()
-        fit_info = retriever.get_default_fit_info(**self.params)
-
-        # Fit for the stellar radius and planetary mass using Gaussian priors.  This
-        # is a way to account for the uncertainties in the published values
-        fit_info.add_gaussian_fit_param('Rs', 0.02*R_sun)
-        fit_info.add_gaussian_fit_param('Mp', 0.04*M_jup)
-
-        # Fit for other parameters using uniform priors
-        R_guess = 1.4 * R_jup
-        T_guess = 1200
-        fit_info.add_uniform_fit_param('Rp', 0.9*R_guess, 1.1*R_guess)
-        fit_info.add_uniform_fit_param('T', 0.5*T_guess, 1.5*T_guess)
-        # fit_info.add_uniform_fit_param("log_scatt_factor", 0, 1)
-        # fit_info.add_uniform_fit_param("logZ", -1, 3)
-        # fit_info.add_uniform_fit_param("log_cloudtop_P", -0.99, 5)
-        # fit_info.add_uniform_fit_param("error_multiple", 0.5, 5)
-
-        # Define bins, depths, and errors
-        wavelengths = 1e-6*np.array([1.119, 1.138, 1.157, 1.175, 1.194, 1.213, 1.232, 1.251, 1.270, 1.288, 1.307, 1.326, 1.345, 1.364, 1.383, 1.401, 1.420, 1.439, 1.458, 1.477, 1.496, 1.515, 1.533, 1.552, 1.571, 1.590, 1.609, 1.628])
-        bins = [[w-0.0095e-6, w+0.0095e-6] for w in wavelengths]
-        depths = 1e-6 * np.array([14512.7, 14546.5, 14566.3, 14523.1, 14528.7, 14549.9, 14571.8, 14538.6, 14522.2, 14538.4, 14535.9, 14604.5, 14685.0, 14779.0, 14752.1, 14788.8, 14705.2, 14701.7, 14677.7, 14695.1, 14722.3, 14641.4, 14676.8, 14666.2, 14642.5, 14594.1, 14530.1, 14642.1])
-        errors = 1e-6 * np.array([50.6, 35.5, 35.2, 34.6, 34.1, 33.7, 33.5, 33.6, 33.8, 33.7, 33.4, 33.4, 33.5, 33.9, 34.4, 34.5, 34.7, 35.0, 35.4, 35.9, 36.4, 36.6, 37.1, 37.8, 38.6, 39.2, 39.9, 40.8])
-
         # Run nested sampling
-        result = retriever.run_multinest(bins, depths, errors, fit_info, plot_best=True)
+        result = self.retriever.run_multinest(self.bins, self.depths, self.errors, self.fit_info, plot_best=True)
         print(result)
 
         # Do some plotting
         fig = corner.corner(result.samples, weights=result.weights,
                             range=[0.99] * result.samples.shape[1],
-                            labels=fit_info.fit_param_names)
+                            labels=self.fit_info.fit_param_names)
         fig.savefig("emcee_corner.png")
 
     def set_parameters(self, params):
@@ -205,6 +174,7 @@ class PlatonWrapper():
         validate_parameters(params)
         apply_factors(params)
         self.params = params
+        self.fit_info = self.retriever.get_default_fit_info(**self.params)
 
 
 if __name__ == '__main__':
@@ -218,6 +188,28 @@ if __name__ == '__main__':
     # Parse and test the parameter file
     params = parse_parameter_file(args.parameter_file)
 
-    platon_wrapper = PlatonWrapper()
-    platon_wrapper.set_parameters(params)
-    platon_wrapper.retrieve()
+    pw = PlatonWrapper()
+    pw.set_parameters(params)
+
+    # Fit for the stellar radius and planetary mass using Gaussian priors.  This
+    # is a way to account for the uncertainties in the published values
+    pw.fit_info.add_gaussian_fit_param('Rs', 0.02*R_sun)
+    pw.fit_info.add_gaussian_fit_param('Mp', 0.04*M_jup)
+
+    # Fit for other parameters using uniform priors
+    R_guess = 1.4 * R_jup
+    T_guess = 1200
+    pw.fit_info.add_uniform_fit_param('Rp', 0.9*R_guess, 1.1*R_guess)
+    pw.fit_info.add_uniform_fit_param('T', 0.5*T_guess, 1.5*T_guess)
+    # pw.fit_info.add_uniform_fit_param("log_scatt_factor", 0, 1)
+    # pw.fit_info.add_uniform_fit_param("logZ", -1, 3)
+    # pw.fit_info.add_uniform_fit_param("log_cloudtop_P", -0.99, 5)
+    # pw.fit_info.add_uniform_fit_param("error_multiple", 0.5, 5)
+
+    # Define bins, depths, and errors
+    pw.wavelengths = 1e-6*np.array([1.119, 1.138, 1.157, 1.175, 1.194, 1.213, 1.232, 1.251, 1.270, 1.288, 1.307, 1.326, 1.345, 1.364, 1.383, 1.401, 1.420, 1.439, 1.458, 1.477, 1.496, 1.515, 1.533, 1.552, 1.571, 1.590, 1.609, 1.628])
+    pw.bins = [[w-0.0095e-6, w+0.0095e-6] for w in pw.wavelengths]
+    pw.depths = 1e-6 * np.array([14512.7, 14546.5, 14566.3, 14523.1, 14528.7, 14549.9, 14571.8, 14538.6, 14522.2, 14538.4, 14535.9, 14604.5, 14685.0, 14779.0, 14752.1, 14788.8, 14705.2, 14701.7, 14677.7, 14695.1, 14722.3, 14641.4, 14676.8, 14666.2, 14642.5, 14594.1, 14530.1, 14642.1])
+    pw.errors = 1e-6 * np.array([50.6, 35.5, 35.2, 34.6, 34.1, 33.7, 33.5, 33.6, 33.8, 33.7, 33.4, 33.4, 33.5, 33.9, 34.4, 34.5, 34.7, 35.0, 35.4, 35.9, 36.4, 36.6, 37.1, 37.8, 38.6, 39.2, 39.9, 40.8])
+
+    pw.retrieve()
