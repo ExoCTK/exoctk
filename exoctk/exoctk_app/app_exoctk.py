cca5251dc3e66873c3f22e381848e2b997e8c57b
Mees Fix <mfix@stsci.edu>
2019-03-15 11:26:06 -0400

    Added target resolver to the groups and integrations calculator.

--- exoctk/exoctk_app/app_exoctk.py
+++ exoctk/exoctk_app/app_exoctk.py
@@ -328,22 +328,39 @@ def groups_integrations():
     if request.method == 'GET':       
         try: 
             # http://0.0.0.0:5000/groups_integrations?k_mag=10.602&transit_duration=0.0655
+            target_name = request.args.get('target')
             k_mag = request.args.get('k_mag')
             trans_dur = float(request.args.get('transit_duration'))
             # According to Kevin the obs_dur = 3*trans_dur+1 hours
             # transit_dur is in days from exomast, convert first.
             trans_dur *= u.day.to(u.hour)
-            print(type(trans_dur))
             obs_dur = 3*trans_dur + 1
-            print(obs_dur)
             
-            groupIntVars = {'k_mag':k_mag, 
+            groupIntVars = {'tname':target_name,
+                            'k_mag':k_mag, 
                             'obs_dur':obs_dur}
 
             return render_template('groups_integrations.html', sat_data=sat_data, groupIntVars=groupIntVars)
             
         except TypeError:
             return render_template('groups_integrations.html', sat_data=sat_data, groupIntVars={})
+
+    elif request.method == 'POST':
+        if request.form['submit'] == "Resolve Target":
+            target_name = request.form['targetname']
+            data, target_url = get_target_data(target_name, request_url=True)
+            k_mag = data['Kmag']
+            trans_dur = data['transit_duration']
+            # According to Kevin the obs_dur = 3*trans_dur+1 hours
+            # transit_dur is in days from exomast, convert first.
+            trans_dur *= u.day.to(u.hour)
+            obs_dur = 3*trans_dur + 1
+            
+            groupIntVars = {'tname':target_name,
+                            'k_mag':k_mag, 
+                            'obs_dur':obs_dur}
+
+            return render_template('groups_integrations.html', sat_data=sat_data, groupIntVars=groupIntVars)
 
     return render_template('groups_integrations.html', sat_data=sat_data, groupIntVars={})
 
