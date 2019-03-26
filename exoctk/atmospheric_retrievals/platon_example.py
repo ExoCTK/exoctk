76ceb22e88337b5beebe1582665aa3656b771270
Matthew Bourque <bourque@stsci.edu>
2019-03-26 10:39:45 -0400

    Added placeholder for platon_example module.

---
+++ exoctk/atmospheric_retrievals/platon_example.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python
+
+"""This module is not yet implemented, but will serve as a
+comprehensive, robust example of how to use the ``platon_wrapper``
+module in ExoCTK's ``atmopsheric_retrieval`` subpackage.
+"""
