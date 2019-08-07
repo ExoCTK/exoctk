11119eee3a972d83074e1f6c0c34d5071ce8849a
Jules Fowler <jfowler@stsci.edu>
2019-08-07 11:38:48 -0400

    Hot fix for groups and integrations.

--- exoctk/exoctk_app/app_exoctk.py
+++ exoctk/exoctk_app/app_exoctk.py
@@ -272,11 +272,18 @@ def groups_integrations():
 
         # According to Kevin the obs_dur = 3*trans_dur+1 hours
         # transit_dur is in days from exomast, convert first.
-        trans_dur = float(request.args.get('transit_duration'))
-        trans_dur *= u.day.to(u.hour)
-        obs_dur = 3*trans_dur + 1
-        form.obs_duration.data = obs_dur
-        
+        try:
+            trans_dur = float(request.args.get('transit_duration'))
+            trans_dur *= u.day.to(u.hour)
+            obs_dur = 3*trans_dur + 1
+            form.obs_duration.data = obs_dur
+        except TypeError:
+            trans_dur = request.args.get('transit_duration')
+            if trans_dur == None:
+                form.obs_duration.data = trans_dur
+            else:
+                err = 'The Transit Duration from ExoMAST experienced some issues. Try a different spelling or source.'
+                return render_template('groups_integrations_error.html', err=err)
         return render_template('groups_integrations.html', form=form, sat_data=sat_data)
         
     # Reload page with stellar data from ExoMAST
