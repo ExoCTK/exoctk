abe9c43e85931669bf7bb64888e7a52c0710dc99
Jennifer Medina <jmedina@stsci.edu>
2019-08-12 14:02:09 -0400

    using SkyCoord for unit conversion

--- exoctk/contam_visibility/resolve.py
+++ exoctk/contam_visibility/resolve.py
@@ -1,35 +1,6 @@
 import numpy as np
-
+from astropy.coordinates import SkyCoord
 from exoctk.utils import get_target_data
-
-def deg2HMS(ra='', dec='', round=True):
-  RA, DEC, rs, ds = '', '', '', ''
-  if dec:
-    if str(dec)[0] == '-':
-      ds, dec = '-', abs(dec)
-    deg = int(dec)
-    decM = abs(int((dec-deg)*60))
-    if round:
-      decS = np.round(((abs((dec-deg)*60)-decM)*60), 2)
-    else:
-      decS = (abs((dec-deg)*60)-decM)*60
-    DEC = '{0}{1}:{2}:{3}'.format(ds, deg, decM, decS)
-
-  if ra:
-    if str(ra)[0] == '-':
-      rs, ra = '-', abs(ra)
-    raH = int(ra/15)
-    raM = int(((ra/15)-raH)*60)
-    if round:
-      raS = np.round(((((ra/15)-raH)*60)-raM)*60, 2)
-    else:
-      raS = ((((ra/15)-raH)*60)-raM)*60
-    RA = '{0}{1}:{2}:{3}'.format(rs, raH, raM, raS)
-
-  if ra and dec:
-    return (RA, DEC)
-  else:
-    return RA or DEC
 
 def resolve_target(targetName):
     """
@@ -51,6 +22,12 @@ def resolve_target(targetName):
     ra_deg = data['RA']
     dec_deg = data['DEC']
 
-    ra_hms, dec_hms = deg2HMS(ra=ra_deg, dec=dec_deg)
+    sc = SkyCoord(ra_deg, dec_deg, unit='deg')
+    ra_hms = '{}:{}:{}'.format(int(sc.ra.hms[0]), \
+                               int(sc.ra.hms[1]), \
+                               np.round(sc.ra.hms[2], 2))
+    dec_hms = '{}:{}:{}'.format(int(sc.dec.dms[0]), \
+                                int(sc.dec.dms[1]), \
+                                np.round(sc.dec.dms[2], 2))
 
     return ra_hms, dec_hms
