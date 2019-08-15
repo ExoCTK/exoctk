f7f06b9f43ac29317858934796aa398346571081
Jennifer Medina <jmedina@stsci.edu>
2019-08-15 11:41:20 -0400

    putting zapped changes back in

--- exoctk/contam_visibility/visibilityPA.py
+++ exoctk/contam_visibility/visibilityPA.py
@@ -16,7 +16,7 @@ import pkg_resources
 from astropy.table import Table
 from astropy.time import Time
 from bokeh.plotting import figure, ColumnDataSource
-from bokeh.models import HoverTool
+from bokeh.models import HoverTool, ranges
 from bokeh.models.widgets import Panel, Tabs
 import matplotlib.dates as mdates
 import numpy as np
@@ -331,6 +331,7 @@ def using_gtvt(ra, dec, instrument, ephFileName=None, output='bokeh'):
     # Plot formatting
     fig.xaxis.axis_label = 'Date'
     fig.yaxis.axis_label = 'Position Angle (degrees)'
+    fig.y_range = ranges.Range1d(0, 360)
 
     # Making the output table
     # Creating new lists w/o the NaN values
