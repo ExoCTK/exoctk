8543e2bd7ff1184e04032398b10779b270675981
Matthew Bourque <bourque@stsci.edu>
2019-08-19 20:41:42 -0400

    Turns out the home directory for travis is platform dependent.  Alas.

--- exoctk/utils.py
+++ exoctk/utils.py
@@ -33,7 +33,7 @@ VERSION = '0.2'
 EXOCTK_DATA = os.environ.get('EXOCTK_DATA')
 
 # If the variable is blank or doesn't exist
-ON_TRAVIS = os.path.expanduser('~') == '/home/travis'
+ON_TRAVIS = os.path.expanduser('~') == '/home/travis' or os.path.expanduser('~') == '/Users/travis'
 if not ON_TRAVIS:
     if not EXOCTK_DATA:
         raise ValueError(
