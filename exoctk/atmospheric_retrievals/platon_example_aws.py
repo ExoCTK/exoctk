60cf267ed1bd34f107f46bb1bb74ffefb7adc7e7
Matthew Bourque <bourque@stsci.edu>
2019-09-13 11:19:15 -0400

    Added longer example

--- exoctk/atmospheric_retrievals/platon_example_aws.py
+++ exoctk/atmospheric_retrievals/platon_example_aws.py
@@ -22,9 +22,12 @@ Use
     function and pass as a parameter which example to run.  Available
     examples include:
 
-        from platon_example_aws import example_aws
+        from platon_example_aws import example_aws, example_aws_long
         example_aws('emcee')
         example_aws('multinest')
+        example_aws_long('emcee')
+        example_aws_long('multinest')
+
 
 Dependencies
 ------------
@@ -120,7 +123,65 @@ def example_aws(method):
     pw.retrieve(method)
 
 
+def example_aws_long(method):
+    """Performs an longer example run of the ``emcee`` and
+    `multinest`` retrievals using AWS
+
+    Parameters
+    ----------
+    method : str
+        The method to use to perform the atmopsheric retrieval; can
+        either be ``multinest`` or ``emcee``
+    """
+
+    ssh_file = get_config()['ssh_file']
+    ec2_id = get_config()['ec2_id']
+
+    # Define the fit parameters
+    params = {
+        'Rs': 1.19,  # Required
+        'Mp': 0.73,  # Required
+        'Rp': 1.4,  # Required
+        'T': 1200.0,  # Required
+        'logZ': 0,  # Optional
+        'CO_ratio': 0.53,  # Optional
+        'log_cloudtop_P': 4,  # Optional
+        'log_scatt_factor': 0,  # Optional
+        'scatt_slope': 4,  # Optional
+        'error_multiple': 1,  # Optional
+        'T_star': 6091}  # Optional
+
+    # Initialize the object, set parameters, and perform retreival
+    pw = PlatonWrapper()
+    pw.set_parameters(params)
+
+    # Fit for the stellar radius and planetary mass using Gaussian priors.  This
+    # is a way to account for the uncertainties in the published values
+    pw.fit_info.add_gaussian_fit_param('Rs', 0.02*R_sun)
+    pw.fit_info.add_gaussian_fit_param('Mp', 0.04*M_jup)
+
+    # Fit for other parameters using uniform priors
+    R_guess = 1.4 * R_jup
+    T_guess = 1200
+    pw.fit_info.add_uniform_fit_param('Rp', 0.9*R_guess, 1.1*R_guess)
+    pw.fit_info.add_uniform_fit_param('T', 0.5*T_guess, 1.5*T_guess)
+    pw.fit_info.add_uniform_fit_param("log_scatt_factor", 0, 1)
+    pw.fit_info.add_uniform_fit_param("logZ", -1, 3)
+    pw.fit_info.add_uniform_fit_param("log_cloudtop_P", -0.99, 5)
+    pw.fit_info.add_uniform_fit_param("error_multiple", 0.5, 5)
+
+    # Define bins, depths, and errors
+    pw.wavelengths = 1e-6*np.array([1.119, 1.138, 1.157, 1.175, 1.194, 1.213, 1.232, 1.251, 1.270, 1.288, 1.307, 1.326, 1.345, 1.364, 1.383, 1.401, 1.420, 1.439, 1.458, 1.477, 1.496, 1.515, 1.533, 1.552, 1.571, 1.590, 1.609, 1.628])
+    pw.bins = [[w-0.0095e-6, w+0.0095e-6] for w in pw.wavelengths]
+    pw.depths = 1e-6 * np.array([14512.7, 14546.5, 14566.3, 14523.1, 14528.7, 14549.9, 14571.8, 14538.6, 14522.2, 14538.4, 14535.9, 14604.5, 14685.0, 14779.0, 14752.1, 14788.8, 14705.2, 14701.7, 14677.7, 14695.1, 14722.3, 14641.4, 14676.8, 14666.2, 14642.5, 14594.1, 14530.1, 14642.1])
+    pw.errors = 1e-6 * np.array([50.6, 35.5, 35.2, 34.6, 34.1, 33.7, 33.5, 33.6, 33.8, 33.7, 33.4, 33.4, 33.5, 33.9, 34.4, 34.5, 34.7, 35.0, 35.4, 35.9, 36.4, 36.6, 37.1, 37.8, 38.6, 39.2, 39.9, 40.8])
+
+    # Set use for AWS and perform retreival
+    pw.use_aws(ssh_file, ec2_id)
+    pw.retrieve(method)
+
+
 if __name__ == '__main__':
 
-    example_aws('multinest')
-    example_aws('emcee')
+    example_aws_long('multinest')
+    example_aws_long('emcee')
