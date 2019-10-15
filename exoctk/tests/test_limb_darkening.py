b3230b1c611a33ddf4c2f4fdbb7dce6205722eb1
Joe Filippazzo <jfilippazzo@stsci.edu>
2019-10-15 11:08:59 -0400

    Fixed failing assertion error

--- exoctk/tests/test_limb_darkening.py
+++ exoctk/tests/test_limb_darkening.py
@@ -54,7 +54,7 @@ def test_ldc_plot():
 
     # Tabbed plot
     fig = ld_session.plot_tabs()
-    assert str(type(fig)) == "<class 'bokeh.models.widgets.panels.Tabs'>"
+    assert str(type(fig)) in ["<class 'bokeh.models.widgets.panels.Tabs'>", "<class 'bokeh.models.layouts.Tabs'>"]
 
 
 def test_ldc_calculation_no_filter():
