9fcb77a8c990ba44c6255064e158e97bff5afb61
Jules Fowler <jfowler@stsci.edu>
2019-12-13 13:53:22 -0500

    Removing the datacheck + automatic import from the init so that we can run modules without data.

--- exoctk/__init__.py
+++ exoctk/__init__.py
@@ -6,20 +6,20 @@ The Exoplanet Characterization Tool Kit is a collection of packages used to redu
 
 import os
 
-from . import modelgrid
-from . import references
-from . import utils
-from . import contam_visibility
-from . import groups_integrations
-from . import limb_darkening
-from . import lightcurve_fitting
+#from . import modelgrid
+#from . import references
+#from . import utils
+#from . import contam_visibility
+#from . import groups_integrations
+#from . import limb_darkening
+#from . import lightcurve_fitting
 
-try:
-    setup_file = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'setup.py')
-    with open(setup_file, 'r') as f:
-        data = f.readlines()
-    __version__ = [line for line in data if 'version' in line][0].strip().split("version='")[-1].split("'")[0]
+#try:
+#    setup_file = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'setup.py')
+#    with open(setup_file, 'r') as f:
+#        data = f.readlines()
+#    __version__ = [line for line in data if 'version' in line][0].strip().split("version='")[-1].split("'")[0]
 
-except FileNotFoundError:
-    print('Could not determine exoctk version')
-    __version__ = 'null'
+#except FileNotFoundError:
+#    print('Could not determine exoctk version')
+#    __version__ = 'null'
