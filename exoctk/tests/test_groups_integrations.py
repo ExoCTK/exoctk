a941dea586cf252d83c81b47e28efd786b338efb
Matthew Bourque <bourque@stsci.edu>
2020-05-18 16:13:36 -0400

    Re-ordered functions based on alphabetical order, updated docstrings

--- exoctk/tests/test_groups_integrations.py
+++ exoctk/tests/test_groups_integrations.py
@@ -1,4 +1,20 @@
-"""
+"""Tests for the ``groups_integrations.py`` module, as well as various
+tests that ensures the structure and contents of the
+``groups_integrations.json`` file in the ``exoctk_data`` package are
+valid.
+
+Authors
+-------
+
+    - Matthew Bourque, May 2020
+
+Use
+---
+
+    The tests can be executed from the command line as such:
+    ::
+
+        pytest -s test_groups_integrations.py
 """
 
 from decimal import Decimal
@@ -72,110 +88,6 @@ with open(GROUPS_INTEGRATIONS_FILE, 'r') as f:
     DATA = json.load(f)
 
 
-# def test_perform_calculation():
-#     """
-#     """
-
-#     params = {
-#         'ins': 'miri',
-#         'mag': Decimal('8.131'),
-#         'obs_time': Decimal('7.431039999999999'),
-#         'sat_max': Decimal('0.95'),
-#         'sat_mode': 'well',
-#         'time_unit': 'hour',
-#         'band': 'K',
-#         'mod': 'f5v',
-#         'filt': 'lrs',
-#         'subarray': 'slitlessprism',
-#         'filt_ta': 'f560w',
-#         'subarray_ta': 'slitlessprism',
-#         'n_group': 'optimize',
-#         'infile': GROUPS_INTEGRATIONS_FILE
-#     }
-
-#     expected_results = {
-#         'ins': 'miri',
-#         'mag': Decimal('8.131'),
-#         'obs_time': Decimal('7.431039999999999'),
-#         'sat_max': 183972.25,
-#         'sat_mode': 'well',
-#         'time_unit': 'hour',
-#         'band': 'K',
-#         'mod': 'f5v',
-#         'filt': 'lrs',
-#         'subarray': 'slitlessprism',
-#         'filt_ta': 'f560w',
-#         'subarray_ta': 'slitlessprism',
-#         'n_group': 36,
-#         'infile': GROUPS_INTEGRATIONS_FILE,
-#         'n_col': 72,
-#         'n_row': 416,
-#         'n_amp': 4,
-#         'n_reset': 0,
-#         'n_frame': 1,
-#         'n_skip': 0,
-#         't_frame': 0.159,
-#         't_int': 5.725,
-#         't_ramp': 5.566,
-#         'n_int': 4673,
-#         't_exp': 7.225,
-#         't_duration': 7.432,
-#         'obs_eff': 0.972,
-#         'ta_t_frame': 0.15904,
-#         'min_ta_groups': 5,
-#         'max_ta_groups': 3,
-#         't_duration_ta_min': 0.95424,
-#         't_duration_ta_max': 0.63616,
-#         'max_sat_prediction': 182048.689,
-#         'max_sat_ta': 93529.062,
-#         'min_sat_ta': 155881.77
-#     }
-
-#     results = perform_calculation(params)
-
-#     # Ensure the number of keys in each dictionary are the same
-#     assert len(results) == len(expected_results)
-
-#     for item in results:
-
-#         print('Testing {}'.format(item))
-
-#         # Ensure the key names in each dictionary match
-#         assert item in expected_results
-
-#         # Ensure the key values in each dictionary match
-#         # print(results[item])
-#         # print(expected_results[item])
-#         # print(results[item] == expected_results[item])
-
-
-# @pytest.mark.parametrize('key, instrument, filt, subarray, model', EXPECTED_VALUES)
-# def test_sat_keys(key, instrument, filt, subarray, model):
-#     """Ensures the ``sci_sat``, ``ta_sat``, and ``ta_snr`` keys data are
-#     present and properly formatted in the ``groups_integrations.json`` file.
-
-#     Parameters
-#     ----------
-#     key : str
-#         The key of interest.  Can be either ``sci_sat``, ``ta_sat``,
-#         or ``ta_snr``
-#     instrument : str
-#         The instrument of interest
-#     filt : str
-#         The filter of interest
-#     subarray : str
-#         The subarray of interest
-#     model : str
-#         The phoenix model of interest
-#     """
-
-#     assert instrument in DATA[key]
-#     assert filt in DATA[key][instrument]
-#     assert subarray in DATA[key][instrument][filt]
-#     assert model in DATA[key][instrument][filt][subarray]
-#     assert len(DATA[key][instrument][filt][subarray][model]) == len(MAGNITUDES)
-
-
 @pytest.mark.parametrize('key', ['sci_sat', 'mags', 'fullwell', 'frame_time', 'ta_sat', 'ta_snr'])
 def test_groups_integrations_keys(key):
     """Ensures that the necessary first-order keys are in the
@@ -188,37 +100,6 @@ def test_groups_integrations_keys(key):
     """
 
     assert key in DATA
-
-
-@pytest.mark.parametrize('magnitude', MAGNITUDES)
-def test_mags_key(magnitude):
-    """Ensures the contents of the ``mags`` key is present in the
-    ``groups_integrations.json`` file are present and match expected
-    values.
-
-    Parameters
-    ----------
-    magnitude : float
-        The magnitude of interest
-    """
-
-    assert magnitude in DATA['mags']
-
-
-@pytest.mark.parametrize('instrument', INSTRUMENTS)
-def test_fullwell_key(instrument):
-    """Ensures the contents of the ``fullwell`` key in the
-    ``groups_integrations.json`` file are present and match expected
-    values.
-
-    Parameters
-    ----------
-    instrument : str
-        The instrument of interest
-    """
-
-    assert instrument in DATA['fullwell']
-    assert isinstance(DATA['fullwell'][instrument], int) or isinstance(DATA['fullwell'][instrument], float)
 
 
 @pytest.mark.parametrize('mode, instrument, subarray', FRAME_TIME_EXPECTED_VALUES)
@@ -245,3 +126,137 @@ def test_frame_time_key(mode, instrument, subarray):
     elif mode == 'ta':
         assert subarray in DATA['frame_time'][instrument]['ta']
         assert isinstance(DATA['frame_time'][instrument]['ta'][subarray], float)
+
+
+@pytest.mark.parametrize('instrument', INSTRUMENTS)
+def test_fullwell_key(instrument):
+    """Ensures the contents of the ``fullwell`` key in the
+    ``groups_integrations.json`` file are present and match expected
+    values.
+
+    Parameters
+    ----------
+    instrument : str
+        The instrument of interest
+    """
+
+    assert instrument in DATA['fullwell']
+    assert isinstance(DATA['fullwell'][instrument], int) or isinstance(DATA['fullwell'][instrument], float)
+
+
+@pytest.mark.parametrize('magnitude', MAGNITUDES)
+def test_mags_key(magnitude):
+    """Ensures the contents of the ``mags`` key is present in the
+    ``groups_integrations.json`` file are present and match expected
+    values.
+
+    Parameters
+    ----------
+    magnitude : float
+        The magnitude of interest
+    """
+
+    assert magnitude in DATA['mags']
+
+
+def test_perform_calculation():
+    """Tests the ``perform_calculation`` function in
+    ``groups_integrations.py``
+    """
+
+    params = {
+        'ins': 'miri',
+        'mag': Decimal('8.131'),
+        'obs_time': Decimal('7.431039999999999'),
+        'sat_max': Decimal('0.95'),
+        'sat_mode': 'well',
+        'time_unit': 'hour',
+        'band': 'K',
+        'mod': 'f5v',
+        'filt': 'lrs',
+        'subarray': 'slitlessprism',
+        'filt_ta': 'f560w',
+        'subarray_ta': 'slitlessprism',
+        'n_group': 'optimize',
+        'infile': GROUPS_INTEGRATIONS_FILE
+    }
+
+    expected_results = {
+        'ins': 'miri',
+        'mag': Decimal('8.131'),
+        'obs_time': Decimal('7.431039999999999'),
+        'sat_max': 183972.25,
+        'sat_mode': 'well',
+        'time_unit': 'hour',
+        'band': 'K',
+        'mod': 'f5v',
+        'filt': 'lrs',
+        'subarray': 'slitlessprism',
+        'filt_ta': 'f560w',
+        'subarray_ta': 'slitlessprism',
+        'n_group': 36,
+        'infile': GROUPS_INTEGRATIONS_FILE,
+        'n_col': 72,
+        'n_row': 416,
+        'n_amp': 4,
+        'n_reset': 0,
+        'n_frame': 1,
+        'n_skip': 0,
+        't_frame': 0.159,
+        't_int': 5.725,
+        't_ramp': 5.566,
+        'n_int': 4673,
+        't_exp': 7.225,
+        't_duration': 7.432,
+        'obs_eff': 0.972,
+        'ta_t_frame': 0.15904,
+        'min_ta_groups': 5,
+        'max_ta_groups': 3,
+        't_duration_ta_min': 0.95424,
+        't_duration_ta_max': 0.63616,
+        'max_sat_prediction': 182048.689,
+        'max_sat_ta': 93529.062,
+        'min_sat_ta': 155881.77
+    }
+
+    results = perform_calculation(params)
+
+    # Ensure the number of keys in each dictionary are the same
+    assert len(results) == len(expected_results)
+
+    for item in results:
+
+        # Ensure the key names in each dictionary match
+        assert item in expected_results
+
+        # Ensure the key values in each dictionary match
+        # print(results[item])
+        # print(expected_results[item])
+        # print(results[item] == expected_results[item])
+
+
+@pytest.mark.parametrize('key, instrument, filt, subarray, model', EXPECTED_VALUES)
+def test_sat_keys(key, instrument, filt, subarray, model):
+    """Ensures the ``sci_sat``, ``ta_sat``, and ``ta_snr`` keys data are
+    present and properly formatted in the ``groups_integrations.json`` file.
+
+    Parameters
+    ----------
+    key : str
+        The key of interest.  Can be either ``sci_sat``, ``ta_sat``,
+        or ``ta_snr``
+    instrument : str
+        The instrument of interest
+    filt : str
+        The filter of interest
+    subarray : str
+        The subarray of interest
+    model : str
+        The phoenix model of interest
+    """
+
+    assert instrument in DATA[key]
+    assert filt in DATA[key][instrument]
+    assert subarray in DATA[key][instrument][filt]
+    assert model in DATA[key][instrument][filt][subarray]
+    assert len(DATA[key][instrument][filt][subarray][model]) == len(MAGNITUDES)
