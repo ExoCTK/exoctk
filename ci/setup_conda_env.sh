5a506ccdcfa39e953806483ae3321a50f9d3826a
Matthew Bourque <bourque@stsci.edu>
2020-05-21 16:39:04 -0400

    Adding conda init bash

--- ci/setup_conda_env.sh
+++ ci/setup_conda_env.sh
@@ -2,4 +2,5 @@
 echo "Creating conda environment for Python $PYTHON_VERSION"
 conda env create -f "env/environment-${PYTHON_VERSION}.yml" || exit 1
 export CONDA_ENV=exoctk-$PYTHON_VERSION
+conda init bash
 conda activate $CONDA_ENV
