34709193246c4ce616ee0b0de7842f3e79f5c394
Matthew Bourque <bourque@stsci.edu>
2020-05-21 16:31:59 -0400

    Trying "conda activate" instead of "source activate"

--- ci/setup_conda_env.sh
+++ ci/setup_conda_env.sh
@@ -2,4 +2,4 @@
 echo "Creating conda environment for Python $PYTHON_VERSION"
 conda env create -f "env/environment-${PYTHON_VERSION}.yml" || exit 1
 export CONDA_ENV=exoctk-$PYTHON_VERSION
-source activate $CONDA_ENV
+conda activate $CONDA_ENV
