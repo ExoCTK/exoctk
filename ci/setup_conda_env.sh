5c958f980c69efe3f6612f35926a6d87ff6e7f5c
Matthew Bourque <bourque@stsci.edu>
2020-05-22 11:46:02 -0400

    Switching back to source activate

--- ci/setup_conda_env.sh
+++ ci/setup_conda_env.sh
@@ -2,5 +2,6 @@
 echo "Creating conda environment for Python $PYTHON_VERSION"
 conda env create -f "env/environment-${PYTHON_VERSION}.yml" || exit 1
 export CONDA_ENV=exoctk-$PYTHON_VERSION
-conda init bash
-conda activate $CONDA_ENV
+source activate $CONDA_ENV
+echo $PATH
+which conda
