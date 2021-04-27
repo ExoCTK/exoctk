a6c222a3434900fec50bc54b238f7c4aef682abb
Joe Filippazzo <jfilippazzo@stsci.edu>
2021-04-27 15:15:18 -0400

    optimizing contam tool but need to fix data sources.

--- exoctk/contam_visibility/resolve.py
+++
@@ -1,9 +0,0 @@
-from exoctk.utils import get_target_data
-
-
-def resolve_target(targetName):
-    data = get_target_data(targetName)
-    ra = data['RA']
-    dec = data['DEC']
-
-    return ra, dec
