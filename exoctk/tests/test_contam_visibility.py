1a0c432b3b9259250c5b67881ce32b022ef13000
Joe Filippazzo <jfilippazzo@stsci.edu>
2021-07-30 11:37:53 -0400

    Renamed contam test module and added field_simulation tests

--- exoctk/tests/test_contam_visibility.py
+++ exoctk/tests/test_contam_visibility.py
@@ -2,7 +2,13 @@ import sys
 
 import pytest
 
-from exoctk.contam_visibility import visibilityPA
+from exoctk.contam_visibility import visibilityPA, field_simulator
+
+
+def test_field_simulator():
+    """Test to see that field_simulator.py works for all modes"""
+    for mode, aper in field_simulator.APERTURES.items():
+        sim, plt = field_simulator.field_simulation("04 25 29.0162", "-30 36 01.603", mode, nPA=5)
 
 
 @pytest.mark.skipif(sys.version_info > (3, 9), reason='jwst_gtvt does not currently support python>=3.9.')
