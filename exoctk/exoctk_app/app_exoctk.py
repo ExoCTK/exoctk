47d58bdcf82cf0f626dc3b8ba176be79a8e6a4a2
york@stsci.edu <york@stsci.edu>
2025-09-15 16:50:49 -0400

    Updated code to not save source files (to avoid taking up too much space)

--- exoctk/exoctk_app/app_exoctk.py
+++ exoctk/exoctk_app/app_exoctk.py
@@ -3,6 +3,8 @@ import io
 import json
 import os
 from pkg_resources import resource_filename
+import redis
+from rq import push_connection, pop_connection, Queue
 import tempfile
 from datetime import datetime
 
@@ -13,11 +15,11 @@ import astropy.units as u
 from bokeh.embed import components
 from bokeh.resources import INLINE
 import flask
-from flask import Flask, make_response, render_template, Response, request, send_file, session
-import form_validation as fv
+from flask import Flask, make_response, render_template, Response, request, send_file, session, g, current_app
 import numpy as np
 
 from exoctk import log_exoctk
+from exoctk.exoctk_app import form_validation as fv
 from exoctk.contam_visibility.new_vis_plot import build_visibility_plot, get_exoplanet_positions
 from exoctk.contam_visibility import field_simulator as fs
 from exoctk.contam_visibility import contamination_figure as cf
@@ -37,6 +39,11 @@ app_exoctk = Flask(__name__)
 # define the cache config keys, remember that it can be done in a settings file
 app_exoctk.config['CACHE_TYPE'] = 'null'
 app_exoctk.config['SECRET_KEY'] = 'Thisisasecret!'
+app_exoctk.config['REDIS_URL'] = 'redis://redis:6379/0'
+app_exoctk.config['QUEUES'] = ['default']
+
+redis_url = app.config['REDIS_URL']
+
 
 # Load the database to log all form submissions
 if get_env_variables()['exoctklog_dir'] is None:
@@ -340,6 +347,35 @@ def pa_contam():
 
     """
     return render_template('pa_contam.html')
+
+
+@rq.job(queue="default")
+def _field_simulator(ra, dec, inst, date, comp):
+    """
+    Dispatch the contamination visibility task using RQ, and return the task information.
+    """
+    # Make field simulation
+    targframe, starcube, results = fs.field_simulation(
+        ra, dec, inst, target_date=date, binComp=comp, plot=False, multi=False
+    )
+    return targframe, starcube, results
+
+
+@app_exoctk.route('/status/<job_id>')
+def job_status(job_id):
+    job = rq.
+    q = Queue()
+    job = q.fetch_job(job_id)
+    if job is None:
+        response = {'status': 'unknown'}
+    else:
+        response = {
+            'status': job.get_status(),
+            'result': job.result,
+        }
+        if job.is_failed:
+            response['message'] = job.exc_info.strip().split('\n')[-1]
+    return jsonify(response)
 
 
 @app_exoctk.route('/contam_visibility', methods=['GET', 'POST'])
