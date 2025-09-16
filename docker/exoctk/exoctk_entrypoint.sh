5387a76372b5e670da1b61b6be121cfb922d9e00
york@stsci.edu <york@stsci.edu>
2025-09-16 12:51:48 -0400

    Adding bind option

--- docker/exoctk/exoctk_entrypoint.sh
+++ docker/exoctk/exoctk_entrypoint.sh
@@ -11,4 +11,4 @@ done
 # python /exoctk/exoctk/exoctk/exoctk_app/app_exoctk.py
 
 # run gunicorn
-gunicorn -w 4 'exoctk.exoctk_app.app_exoctk:app_exoctk'
+gunicorn -w 4 -b 0.0.0.0 'exoctk.exoctk_app.app_exoctk:app_exoctk'
