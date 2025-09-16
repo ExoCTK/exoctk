767350cb4e0956d4b7e1b50f0b87bac0521642d1
york@stsci.edu <york@stsci.edu>
2025-09-16 12:41:52 -0400

    Moved app from Flask to multi-worker gunicorn

--- docker/exoctk/exoctk_entrypoint.sh
+++ docker/exoctk/exoctk_entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-until cd /exoctk/exoctk/exoctk/exoctk_app
+until cd /exoctk/exoctk
 do
     echo "Waiting for server volume..."
 done
 
 # run flask
-flask --app app_exoctk run
+# flask --app app_exoctk run
 
 # python /exoctk/exoctk/exoctk/exoctk_app/app_exoctk.py
+
+# run gunicorn
+gunicorn -w 4 'exoctk.exoctk_app.app_exoctk:app_exoctk'
