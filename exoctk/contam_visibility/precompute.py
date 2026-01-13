cb02a08da8d5be9526b01a1eaf85a8d2c5c0008c
Joe Filippazzo <jfilippazzo@stsci.edu>
2026-01-13 15:08:30 -0500

    Added code to generate a precomputed database

--- exoctk/contam_visibility/precompute.py
+++ exoctk/contam_visibility/precompute.py
@@ -14,9 +14,11 @@ print(data["orbital_params"].shape)   # (15,)
 print(data["target_trace"].shape)     # (256, 2048)
 print(data["contamination"].shape)    # (360, 256, 2048)
 """
-import h5py
 import numpy as np
 import requests
+
+from . import field_simulator as fs
+from ..utils import get_target_data, get_canonical_name
 
 
 def fetch_exoplanet_names_eaot():
@@ -31,7 +33,7 @@ def fetch_exoplanet_names_eaot():
     return names
 
 
-def generate_exoplanet_file(filename: str):
+def generate_exoplanet_file(filename):
     """
     Create an HDF5 file with exoplanet data structures but no data... yet.
 
@@ -65,33 +67,69 @@ def generate_exoplanet_file(filename: str):
 
     print(f"Finished! Saved data for {count}/{len(planet_names)} exoplanets to {filename}.")
 
+    return planet_names
 
-def load_exoplanet(filename: str, name: str) -> dict:
+
+def save_exoplanet_data(filename, exoplanet_name, target_trace, contamination):
+        """
+        Save target trace and contamination data for a single exoplanet.
+
+        Parameters
+        ----------
+        filename : str
+            Path to HDF5 file
+        exoplanet_name : str
+            Exoplanet name (EAOT name)
+        target_trace : ndarray
+            2D array of shape (256, 2048)
+        contamination : ndarray
+            3D array of shape (360, 256, 2048)
+        """
+        # Open the file and save the data
+        with h5py.File(filename, "r+") as f:
+            if exoplanet_name not in f:
+                raise KeyError(f"Exoplanet '{exoplanet_name}' not found in {filename}")
+
+            grp = f[group_name]
+
+            # Write data in-place (no reallocation)
+            grp["target_trace"][:, :] = target_trace
+            grp["contamination"][:, :, :] = contamination
+
+            # Optional provenance
+            grp.attrs["filled"] = True
+
+        print(f"{exoplanet_name} data saved to {filename}")
+
+
+def generate_database(filename, aperture='NIS_SUBSTRIP256'):
     """
-    Load exoplanet data by name.
+    Compute the contamination data and save to the file
 
     Parameters
     ----------
-    filename : str
+    filename: str
         Path to HDF5 file
-    name : str
-        Exoplanet name (group key)
-
-    Returns
-    -------
-    dict
-        Dictionary containing exoplanet data
+    aperture:
+        The aperture to use, ['NIS_SUBSTRIP96', 'NIS_SUBSTRIP256', 'NRCA5_GRISM256_F444W', 'NRCA5_GRISM256_F322W2', 'MIRI_SLITLESSPRISM']
     """
-    with h5py.File(filename, "r") as f:
-        if name not in f:
-            raise KeyError(f"Exoplanet '{name}' not found in {filename}")
+    # Make the empty file
+    planet_names = generate_exoplanet_file(filename)
 
-        # Get the data
-        grp = f[name]
-        data = {"name": grp.attrs["name"],
-                "target_trace": grp["target_trace"][:],
-                "contamination": grp["contamination"][:]}
+    with h5py.File(filename, "w") as f:
 
-        # TODO: Add slicing for PA ranges to speed up retrieval?
+        for targname in planet_names:
 
-        return data
+            # Get the data by name
+            targname = get_canonical_name(targname)
+            data, _ = get_target_data(targname)
+
+            # Update the coordinates
+            ra_deg = data.get('RA')
+            dec_deg = data.get('DEC')
+
+            # Run contam tool
+            target_trace, contamination, _ = fs.field_simulation(ra_deg, dec_deg, aperture, plot=False)
+
+            # Save the data to file
+            save_exoplanet_data(filename, exoplanet_name, target_trace, contamination)
