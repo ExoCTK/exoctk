ca9354ba51be5e0f31744aac205dc11e08a61ce3
york@stsci.edu <york@stsci.edu>
2026-07-07 11:20:58 -0400

    Passing in target name so that the cache is ever used

--- exoctk/exoctk_app/app_exoctk.py
+++ exoctk/exoctk_app/app_exoctk.py
@@ -559,6 +559,7 @@ def contam_visibility():
                     'ra': ra_deg,
                     'dec': dec_deg,
                     'aperture': form.inst.data,
+                    'targname': form.targname.data,
                     'target_date': form.epoch.data,
                 }
                 if companion is not None:
@@ -575,6 +576,7 @@ def contam_visibility():
                     'ra': ra_deg,
                     'dec': dec_deg,
                     'aperture': form.inst.data,
+                    'targname': form.targname.data,
                     'target_date': form.epoch.data,
                 }
 
